9db4d679d3460f16e435e77f3b9d37e62e773006
Juan José Londoño Tirado <jjose.londono@udea.edu.co>
2020-03-24 19:19:30 -0500

    Proyecto angular

---
+++ src/app/crud-add/crud-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CrudAddComponent } from './crud-add.component';
+
+describe('CrudAddComponent', () => {
+  let component: CrudAddComponent;
+  let fixture: ComponentFixture<CrudAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CrudAddComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CrudAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
